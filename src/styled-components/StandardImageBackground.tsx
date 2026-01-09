f9db3557bba560443774f3bc7ade81286697f6fd
priscillavassallo <priscillavassallo96@gmail.com>
2026-01-09 20:12:37 -0300

    feat: adiciona componente StandardImageBackground

---
+++ src/styled-components/StandardImageBackground.tsx
@@ -0,0 +1,13 @@
+import styled from "styled-components/native";
+
+export const StandardImageBackground = styled.ImageBackground.attrs({
+    source: require("../assets/images/background.jpg"),
+    imageStyle:{
+        resizeMode:"cover"
+    }
+})
+`
+    flex: 1;
+    justify-content: center;
+    align-items: center;
+`
