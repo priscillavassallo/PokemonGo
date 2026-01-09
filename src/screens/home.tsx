dde8fe4bf5173904affd19f09c00ed89b523fc65
priscillavassallo <priscillavassallo96@gmail.com>
2026-01-09 20:46:30 -0300

    feat: adiciona tela Home

---
+++ src/screens/home.tsx
@@ -0,0 +1,45 @@
+import { View } from "react-native";
+import styled from "styled-components/native";
+import { StandardImageBackground } from "../styled-components/StandardImageBackground";
+
+const Title = styled.Text`
+  color: red;
+  font-family: ComicReliefBold;
+`
+const Logo = styled.Image.attrs({
+  source: require("../assets/images/pokemongo-logo.png"),
+  resizeMode: "contain",
+})`
+  width: 50%;
+`
+const ScanButton = styled.TouchableOpacity`
+  background-color: white;
+  padding: 18px 12px;
+`
+
+const TextButton = styled.Text`
+  color: #000;
+  font-family: ComicReliefBold;
+`
+
+export default function Home() {
+  return (
+    <StandardImageBackground>
+
+      <View>
+        <Title> 
+        PRISCILLA VASSALLO 
+        </Title>
+      </View>
+
+      <Logo />
+
+      <ScanButton>
+          <TextButton>
+              Scannear QRCODE
+          </TextButton>
+      </ScanButton>
+
+    </StandardImageBackground>
+  );
+}
